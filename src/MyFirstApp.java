83c3418466d6681b1c372773dd3355aef9f4ded3
bxrxbxzx <ast_16@mail.ru>
2019-03-12 00:06:20 +0300

    cycle, while int smaller, than nine

---
+++ src/MyFirstApp.java
@@ -0,0 +1,14 @@
+public class MyFirstApp {
+
+    public static void main(String[] args) {
+
+        for (int x = 0; x < 9; x = x + 1)
+        System.out.println("Значение: " + x);
+    }
+}
+
+
+//        System.out.println("Я управляю");
+//        System.out.println("миром");
+//    }
+//}
