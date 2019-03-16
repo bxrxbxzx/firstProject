54933cf23ff4c081195b91d17423b2f01b1dd08a
bxrxbxzx <ast_16@mail.ru>
2019-03-16 17:21:53 +0300

    cycle, while

--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -1,9 +1,58 @@
+
 public class MyFirstApp {
 
     public static void main(String[] args) {
 
-        for (int x = 0; x < 9; x = x + 1)
-        System.out.println("Значение: " + x);
+        int x = 1
+;
+        while(x <= 512){
+            System.out.println(x);
+            x = x * 2;
+
+//        for (int x = 0; x < 9; x = x + 1)
+//            System.out.println("Значение: " + x);
+//
+//
+//        short New;
+//        New = 4;
+//        System.out.println("New integer is: " + New);
+//
+//
+//        int x = 4;
+//        while (x >= 3) {
+//            x = x - 1;
+//            System.out.println(x);
+//        }
+//
+//        int x = 1;
+//        System.out.println("Значение x: " + x);
+//
+//        while (x < 3 ) {
+//            System.out.println("Значение х все еще: " + x);
+//            x = x + 1;
+//
+//        }
+//
+//        int a = 5;
+//        while(a <= 100 ){
+//            System.out.println(a);
+//            a += 5; //тоже самое, что и a = a + 5;
+
+
+
+        }
+//      int x =1;
+//      while(x < 3){
+//          System.out.println("DooBee");
+//          x = x + 1;
+//      }
+//      if(x == 3){
+//          System.out.println("Do");
+//          x = x + 1;
+//      }
+//      if (x == 4){
+//            System.out.println("Scooby");
+//        }
     }
 }
 
@@ -12,3 +61,4 @@ public class MyFirstApp {
 //        System.out.println("миром");
 //    }
 //}
+
